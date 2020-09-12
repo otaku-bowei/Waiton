f78931fe31d8b0a722f41ff2b68bc430f4ece1bf
defsoul <252795767@qq.com>
2020-09-12 15:24:50 +0800

    添加店铺相关功能

--- src/main/java/pos/demo/store/Service/StoreService.java
+++ src/main/java/pos/demo/store/Service/StoreService.java
@@ -1,13 +1,29 @@
 package pos.demo.store.Service;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pos.demo.common.DateFormat;
 import pos.demo.store.DAO.StoreMapper;
 
 @Service
 public class StoreService {
     @Autowired
     StoreMapper storeMapper ;
+
+    /*
+    * @author   王柏伟
+    * @param    storeCode   店铺编号
+    * @param    storeName   店铺名称
+    * @param    owerEmail   用户邮箱
+    * @param    owerUsername    用户名
+    * @param    storeType   店铺类型
+    */
+    public void buildStore(String storeCode,String storeName,String owerEmail,
+                           String owerUsername,String storeType){
+
+        storeMapper.buildStore(storeCode,storeName,owerEmail,owerUsername,storeType, DateFormat.getTimeNow());
+    }
 
 
 }
